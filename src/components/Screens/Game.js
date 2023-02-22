37c88dd076ee21b5d42391f1d2dc29822cae5349
Tushar Jain <tusharlock10@Tushars-MacBook-Air.local>
2023-02-22 15:03:28 +0530

    rendered image

--- src/components/Screens/Game.js
+++ src/components/Screens/Game.js
@@ -3,10 +3,18 @@ import React from "react";
 class Game extends React.Component {
   render() {
     return (
-      <div style={{justifyContent:'center', alignContent:'center'}}>
+      <div style={{
+      backgroundImage:`url('https://media.discordapp.net/attachments/1012398048745373697/1076917548639059988/FirstMap.png')`,
+      height:'100vh',
+      width:'100vw',
+      backgroundSize:'cover',
+      backgroundRepeat:'no-repeat',
+      }}>
       </div>
     )
   }
 }
 
 export default Game;
+
+//backgroundImage: `url('https://media.discordapp.net/attachments/1012398048745373697/1076917548639059988/FirstMap.png')`,

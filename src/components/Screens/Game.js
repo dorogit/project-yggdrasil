82c2562550f9f1aa9d0c315ba1749a231ea806fe
Tushar Jain <tusharlock10@Tushars-MacBook-Air.local>
2023-02-22 17:50:25 +0530

    test

--- src/components/Screens/Game.js
+++ src/components/Screens/Game.js
@@ -1,20 +1,39 @@
 import React from "react";
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div style={{
-      backgroundImage:`url('https://media.discordapp.net/attachments/1012398048745373697/1076917548639059988/FirstMap.png')`,
-      height:'100vh',
-      width:'100vw',
-      backgroundSize:'cover',
-      backgroundRepeat:'no-repeat',
-      }}>
-      </div>
-    )
+import { useRef, useEffect } from 'react';
+const Game = () => {
+  const canvasRef = useRef(null);
+  const drawImage = (ctx) => {
+    const img = new Image();
+    img.src = 'https://media.discordapp.net/attachments/1012398048745373697/1077916410833022976/test-map.png?width=1026&height=1026'
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+    }
   }
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    drawImage(ctx);
+  }, []);
+  
+  return (
+    <div style={{display:'flex', justifyContent:'center',alignItems:'center'}}>
+      <canvas ref={canvasRef} width={800} height={800} />
+    </div>
+  );
+  
 }
 
 export default Game;
 
 //backgroundImage: `url('https://media.discordapp.net/attachments/1012398048745373697/1076917548639059988/FirstMap.png')`,
+/*
+<div style={{
+    backgroundImage:`url('https://media.discordapp.net/attachments/1012398048745373697/1076917548639059988/FirstMap.png')`,
+    height:'100vh',
+    width:'100vw',
+    backgroundSize:'cover',
+    backgroundRepeat:'no-repeat',
+    }}>
+    </div>
+*/

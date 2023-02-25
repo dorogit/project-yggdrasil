55b2e1cea1cfb6fe982c717749dbc9d1f5ce53ed
Tushar Jain <tusharlock10@Tushars-MacBook-Air.local>
2023-02-25 13:13:56 +0530

    movement

--- src/components/Screens/Game.js
+++ src/components/Screens/Game.js
@@ -1,6 +1,8 @@
 import React from "react";
 import mapImage from '../../assets/test-map.png'
 import characterImage from '../../assets/Character/character-1.png'
+import collisionBox from '../../assets/collision.png'
+import array from '../../assets/collision.json'
 class Game extends React.Component {
 
   constructor(props) {
@@ -8,6 +10,7 @@ class Game extends React.Component {
     this.canvasRef = React.createRef()
     this.mapX = -585
     this.mapY = -50
+    this.collisionArray = []
   }
 
   componentDidMount() {
@@ -17,18 +20,22 @@ class Game extends React.Component {
           this.mapY +=20
           break;
         case "a":
-          this.mapX -=20
+          this.mapX +=20
           break;
         case "s":
           this.mapY -=20
           break; 
         case "d":
-          this.mapX +=20
+          this.mapX -=20
           break;
         default:
           break
       }
     })
+    for(let i=0; i<array.collision.length;i+=50) {
+      this.collisionArray.push(array.collision.slice(i,50+i))
+    }
+    
   }
 
   render() {
@@ -37,8 +44,10 @@ class Game extends React.Component {
       const drawImage = (ctx) => {
         const map = new Image();
         const player_1 = new Image();
+        const collision = new Image();
         map.src = mapImage
         player_1.src = characterImage
+        collision.src = collisionBox
         map.onload = () => {
           ctx.drawImage(map, this.mapX, this.mapY)
           player_1.onload = () => {
@@ -51,7 +60,18 @@ class Game extends React.Component {
               986 / 2 - player_1.height/11,
               player_1.width/8,
               player_1.height/11
-              )
+            )
+            this.collisionArray.forEach((row,index)=>{
+              row.forEach((coords, index2) => {
+                if (coords === 1645) {
+                  ctx.drawImage(
+                    collision,
+                    (index2 * 56)-586,
+                    (index * 56)-50
+                  )
+                }
+              })
+            })
           }
           }
         }
@@ -59,7 +79,7 @@ class Game extends React.Component {
         const ctx = canvas.getContext('2d');
         drawImage(ctx)
       
-    }, 1000);
+    }, 5);
     return (
       <div style={{display:'flex', justifyContent:'center',alignItems:'center'}}>
         <canvas ref= {this.canvasRef} width={1800} height={986} />

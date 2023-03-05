2f6521208020060258b683db5c121ac2a21fc100
Tushar Jain <tusharlock10@Tushars-MacBook-Air.local>
2023-03-05 23:07:32 +0530

    xd

--- src/components/Screens/Game.js
+++ src/components/Screens/Game.js
@@ -1,7 +1,7 @@
 import React from "react";
 import mapImage from '../../assets/test-map.png'
 import characterImage from '../../assets/Character/character-1.png'
-import collisionBox from '../../assets/collision.png'
+import collisionBox from '../../assets/testing.png'
 import array from '../../assets/collision.json'
 class Game extends React.Component {
 
@@ -19,49 +19,110 @@ class Game extends React.Component {
       }
     }
     this.collisionArray = []
+    this.inCollision = false
+    this.lastKeyPressed = null
+    this.playerFrames = 0
+    this.moving = false
   }
 
   componentDidMount() {
     window.addEventListener('keydown', (e) => {
+
+      if (["w","a","s","d"].includes(e.key)) {
+        if (this.moving!==true) {this.moving = true}
+      }
       switch (e.key) {
         case "w":
-          Object.entries(this.movables).forEach(([key, entry])=> {
-            entry.y += 20
-          })
+          if (this.inCollision === false || (this.inCollision === true && this.lastKeyPressed !== "w")) {
+            this.movePlayer(1)
+          }
+          this.lastKeyPressed = "w"
           break;
         case "a":
-          Object.entries(this.movables).forEach(([key, entry])=> {
-            entry.x += 20
-          })
+          if (this.inCollision === false || (this.inCollision === true && this.lastKeyPressed !== "a")) {
+            this.movePlayer(2)
+          }
+          this.lastKeyPressed = "a"
           break;
         case "s":
-          Object.entries(this.movables).forEach(([key, entry])=> {
-            entry.y -= 20
-          })
+          if (this.inCollision === false || (this.inCollision === true && this.lastKeyPressed !== "s")) {
+            this.movePlayer(3)
+          }
+          this.lastKeyPressed = "s"
           break; 
         case "d":
-          Object.entries(this.movables).forEach(([key, entry])=> {
-            entry.x -= 20
-          })
+          if (this.inCollision === false || (this.inCollision === true && this.lastKeyPressed !== "d")) {
+            this.movePlayer(4)
+          }
+          this.lastKeyPressed = "d"
           break;
         default:
-          break
       }
     })
+
+    window.addEventListener('keyup', (e) => {
+      if (this.moving === true && ["w","a","s","d"].includes(e.key) ) {
+        this.playerFrames = 0
+        this.moving = false
+      }
+    })
+  
     for(let i=0; i<array.collision.length;i+=50) {
       this.collisionArray.push(array.collision.slice(i,50+i))
     }
     
   }
 
-  playerColliding(collisionX, collisionY) {
-    //900 and 493 are player's coordinates in the middle of the screen 
-    if (920 >= collisionX && collisionX>= 810 && 490>=collisionY && collisionY>=360) {
-      return true;
+  movePlayer(direction) {
+    this.inCollision = false //if the player moves, he must be out of collision
+    if (this.moving) {
+      switch (direction) {
+        case 1:
+          Object.entries(this.movables).forEach(([key, entry])=> {
+            entry.y += 20
+          })
+          break;
+        case 2:
+          Object.entries(this.movables).forEach(([key, entry])=> {
+            entry.x += 20
+          })
+          break;
+        case 3:
+          Object.entries(this.movables).forEach(([key, entry])=> {
+            entry.y -= 20
+          })
+          break;
+        case 4:
+          Object.entries(this.movables).forEach(([key, entry])=> {
+            entry.x -= 20
+          })
+          break;
+        default:
+          break;
+      }
     }
   }
 
+  checkCollision(collisionX, collisionY) {
+    //900 and 493 are player's coordinates in the middle of the screen 
+    if (920 >= collisionX && collisionX>= 810 && 490>=collisionY && collisionY>=360) {
+      this.inCollision = true
+      return true;
+    } 
+  }
+
   render() {
+
+    setInterval(() => {
+      if (this.moving) {
+        if (this.playerFrames < 8) {
+          this.playerFrames+=1
+        } else if (this.playerFrames === 8) {
+          this.playerFrames = 0
+        }
+      }
+    }, 100);
+
     setInterval(() => {
 
       const drawImage = (ctx) => {
@@ -74,16 +135,29 @@ class Game extends React.Component {
         map.onload = () => {
           ctx.drawImage(map, this.movables.map.x, this.movables.map.y)
           player_1.onload = () => {
-            ctx.drawImage(player_1,
-              0,
-              0,
-              player_1.width/8,
-              player_1.height/11,
-              1800 / 2 - player_1.width/8 / 2,
-              986 / 2 - player_1.height/11,
-              player_1.width/8,
-              player_1.height/11
-            )
+            if (this.moving) {
+              ctx.drawImage(player_1,
+                player_1.width/8 * this.playerFrames,
+                player_1.height/11 * 2,
+                player_1.width/8,
+                player_1.height/11,
+                1800 / 2 - player_1.width/8 / 2,
+                986 / 2 - player_1.height/11,
+                player_1.width/8,
+                player_1.height/11
+              )
+            } else {
+              ctx.drawImage(player_1,
+                0,
+                0,
+                player_1.width/8,
+                player_1.height/11,
+                1800 / 2 - player_1.width/8 / 2,
+                986 / 2 - player_1.height/11,
+                player_1.width/8,
+                player_1.height/11
+              )
+            }
             this.collisionArray.forEach((row,index)=>{
               row.forEach((coords, index2) => {
                 if (coords === 1645) {
@@ -92,8 +166,8 @@ class Game extends React.Component {
                     (index2 * 56) + this.movables.collision.x,
                     (index * 56) + this.movables.collision.y
                   )
-                  if (this.playerColliding((index2 * 56) + this.movables.collision.x, (index * 56) + this.movables.collision.y) === true) {
-                    console.log('colliding')
+                  if (this.checkCollision((index2 * 56) + this.movables.collision.x, (index * 56) + this.movables.collision.y)) {
+                    console.log("in collision")
                   }
                 }
               })
@@ -105,7 +179,8 @@ class Game extends React.Component {
         const ctx = canvas.getContext('2d');
         drawImage(ctx)
       
-    }, 17);
+    }, 100)
+    
     return (
       <div style={{display:'flex', justifyContent:'center',alignItems:'center'}}>
         <canvas ref= {this.canvasRef} width={1800} height={986} />
